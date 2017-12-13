4e9e4f23d9bc37a78838c36f7f45337c198c7a34
vanch3d <nicolas.github@calques3d.org>
2017-12-13 14:51:58 +0000

    Added PhpUnit tests (finally!)

---
+++ test/ControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 12/12/2017
+ * Time: 21:02
+ */
+
+
+class ControllerTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     */
+    public function testGetProjectDescriptors()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testIsProjectDefined()
+    {
+
+
+    }
+
+
+}
+
