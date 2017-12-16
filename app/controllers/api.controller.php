07bdf93efa956ae9f046ef06bb0fd6f7d7c08b6d
vanch3d <nicolas.github@calques3d.org>
2017-12-16 22:39:02 +0000

    - a bit of cleaning - fixed bug in Exception output - refactored the publication handlers

--- app/controllers/api.controller.php
+++ app/controllers/api.controller.php
@@ -158,7 +158,7 @@ class APIController extends Controller
 
     /**
      * Return a detailed list (JSON) of all slides associated with the given project
-     * @param $name The ID of the project
+     * @param string $name The ID of the project
      */
     public function getSlidesJSON($name)
     {
@@ -217,237 +217,12 @@ class APIController extends Controller
             $this->outputJSON($error,500);
         }
     }
-	
-	/**
-	 * 
-	 * @param string $id	The unique identifier of the Zotero item
-	 * @param string $format	The output format
-	 * @return mixed:
-	 * @deprecated individual items are retrieved from the collection request
-	 * @see APIController::retrieveZotero
-	 * 
-	 */
-    private function retrieveZoteroItem($id,$format)
-	{
-        $cfg = $this->app->config("nvl-slim.zotero");
-        if (!isset($cfg))
-                throw new Exception("SlideShare API authentication not configured",500);
 
-        $libraryType = 'user'; //user or group
-        $libraryID = $cfg['userID'];
-        $librarySlug = 'all_things_zotero';
-        $apiKey = $cfg['api_key'];
-        $collectionKey = $cfg['collectID'];
-
-        //create a library object to interact with the zotero API
-		$library = new Zotero_Library($libraryType, $libraryID, $librarySlug, $apiKey);
-		
-		//$user = 			$libraryID;					// Zotero user ID
-		//$collection = 		$collectionKey; 			// Collection key (my own stuff)
-		$data = array(
-				'key'=>		$apiKey,					// Zotero API key
-				'format'=>	'atom',						// output format
-				'content'=> $format						// content formats to retrieve
-		);
-		
-		// build the URL for Zotero API
-		$param = http_build_query($data);
-		$url2 = 'https://api.zotero.org/users/'.$libraryID.'/items/'.$id.'?'.$param;
-		
-		// Send the request to the server (bypassing Zotero_Library)
-		$request = Requests::get($url2,array('Zotero-API-Version' => ZOTERO_API_VERSION));
-		// retrieve the feed and parse it
-
-		$entry = Zotero_Lib_Utils::getFirstEntryNode($request->body);
-		
-		$item = new Zotero_Item($entry, $library);
-		$library->items->addItem($item);
-
-		return $item->subContents[$format];	
-	}
-	
-	
-	/**
-	 * Return the path of the cache file for the given bib item
-	 * @param string $name	the ID of the bib to cache
-	 * @return string
-	 */
-	private function getZoteroCacheFilename($name)
-	{
-		$twig = $this->app->view()->getInstance();
-		return $twig->getCache().'/zotero/'.$name;
-	}
-
-	/**
-	 * Write the content of the bib item into the given cache file 
-	 * @param string 	$name
-	 * @param stdClass 	$data
-	 * @throws RuntimeException
-	 */
-	private function writeZoteroCache($name,$data,$loc)
-	{
-		$file = $this->getZoteroCacheFilename($name);
-		$dir = dirname($file);
-		if (!is_dir($dir)) {
-			if (false === @mkdir($dir, 0777, true) && !is_dir($dir)) {
-				throw new RuntimeException(sprintf("Unable to create the cache directory (%s).", $dir));
-			}
-		} elseif (!is_writable($dir)) {
-			throw new RuntimeException(sprintf("Unable to write in the cache directory (%s).", $dir));
-		}
-		
-		@file_put_contents($file, json_encode($data));
-
-        if (isset($loc))
-        {
-            $file = $this->getZoteroCacheFilename("fileindex");
-            $idx = @file_get_contents($file);
-            $data = array();
-            if ($idx)
-                $data = json_decode($idx,true);
-            $data[$loc] = $name;
-            @file_put_contents($file, json_encode($data));
-        }
-	}
-	
-	public function readZoteroCache($name)
-	{
-		$file = $this->getZoteroCacheFilename($name);
-		return json_decode(@file_get_contents($file),true);
-	}
-
-    public function readZoteroFileIndex()
-    {
-        $file = $this->getZoteroCacheFilename('fileindex');
-        return json_decode(@file_get_contents($file),true);
-    }
-	/**
-	 * 
-	 * @param unknown $name
-	 * @param unknown $cslfile
-	 * @return multitype:string
-	 */
-	private function retrieveZotero($name,$cslfile)
-	{
-        $cfg = $this->app->config("nvl-slim.zotero");
-        if (!isset($cfg))
-            throw new Exception("Zotero API authentication not configured",500);
-
-		$libraryType = 'user'; //user or group
-		$libraryID = $cfg['userID'];
-		$librarySlug = 'all_things_zotero';
-        $apiKey = $cfg['api_key'];
-        $collectionKey = $cfg['collectID'];
-		
-		//create a library object to interact with the zotero API
-		$library = new Zotero_Library($libraryType, $libraryID, $librarySlug, $apiKey);
-		
-		$data = array(
-				'key'=>		$apiKey,					// Zotero API key
-				//'tag'=>		'nvl.'.$name,				// tags to look  for
-				'limit'=>	'50',						// max number of items
-				'order'=>	'date',						// sort by field
-				'sort'=>	'desc',						// sort order
-				'format'=>	'atom',						// output format
-				'content'=> 'csljson,rdf_bibliontology,bibtex,coins,json'		// content formats to retrieve
-		);
-		if ($name!='all')
-			$data['tag'] = 'nvl.'.$name;				// tags to look  for
-
-		// build the URL for Zotero API
-		$param = http_build_query($data);
-		$url2 = 'https://api.zotero.org/users/'.$libraryID.'/collections/'.$collectionKey.'/items/top?'.$param;
-
-		// Send the request to the server (bypassing Zotero_Library)
-		$request = Requests::get($url2,array('Zotero-API-Version' => ZOTERO_API_VERSION));
-		
-		// retrieve the feed and parse it
-		$feed = new Zotero_Feed($request->body);
-		$fetchedItems = $library->items->addItemsFromFeed($feed);
-
-
-        // combine the data into a better JSON structure
-		$pubs = array();
-		if (count($fetchedItems))
-		{
-            // Initialise the CSL generator
-            $csl_data ='../app/utils/styles/'.$cslfile;
-            $csl_data = file_get_contents($csl_data);
-            $citeproc = new citeproc($csl_data);
-
-			// extract the CSLJSON object as the basis for the publications
-			// add the COINS object in the output
-			foreach ($fetchedItems as $item)
-			{
-				$tt = json_decode($item->subContents['csljson'],true);
-				$tt['id']=$item->itemKey;
-				// fix the clsjson date
-				$tt['issued']['date-parts'] = array(array($item->year));
-
-
-
-                $loc = $tt['archive_location'];
-				$tt['PDF'] = $this->app->urlFor('publications.named.pdf',array('name'=>$loc));
-
-                $templatePathname = $this->app->view()->getTemplatePathname('publications/papers/'.$loc.'.twig');
-
-                if (is_file($templatePathname))
-                {
-                    $tt['PubReader'] = $this->app->urlFor('publications.named.pubreader',array('name'=>$loc));
-                }
-
-				// extract the coins for outputs 
-				$tt['output']['coins'] = htmlspecialchars_decode($item->subContents['coins']);
-				$tt['output']['bib'] = htmlspecialchars_decode($item->subContents['bibtex']);
-				// extract keywords and project
-				$tags = $item->apiObject['tags'];
-				foreach ($tags as  $tag){
-					if (false === strpos($tag['tag'], 'nvl.'))
-						$tt['keyword'][] = $tag['tag'];
-					else
-					{
-						//$projIdx = APIController::getProjects();
-
-                        $name2= strtolower(substr($tag['tag'], 4));
-                        $projIdx = $this->isProjectDefined($name2);
-
-                        if ($projIdx) {
-                            $tt['project'] = $projIdx;
-                            $tt['project']['url'] = $this->app->urlFor('project.named', array('name' => $name2));
-                        }
-					}
-				}
-
-
-                $kk= json_decode(json_encode($tt));
-                $ref = $citeproc->render($kk);
-                $cite = $citeproc->render($kk,'citation');
-                $tt['output']['cite'] = $cite;
-                $tt['output']['ref'] = $ref;
-
-                $item->subContents['csljson']=json_encode($tt);
-                $pubs[$item->itemKey] = $tt;
-				$this->writeZoteroCache($item->itemKey, $item->subContents,$loc);
-			};
-
-
-			// Format each publication and citation
-			foreach($pubs as &$data) {
-				$kk= json_decode(json_encode($data));
-
-			}
-		}
-		
-		return $pubs;
-	}
-
-	
-	/**
+    /**
 	 * Retrieve the list of publications for the given project (tag).
 	 * The tag is expected to be one of the projects (see ), or 'all' for the complete list.
 	 * 
 	 * @param string $name	The project tag to retrieve publications for
-	 * @return	A JSON object containing the requested publications
 	 */
 	public function getPublicationsJSON($name)
 	{
@@ -456,7 +231,7 @@ class APIController extends Controller
 		$this->app->expires('+1 week');
 
         try {
-            $pubs = $this->retrieveZotero($name, "umuai-nvl.csl");
+            $pubs = $this->retrieveFromZotero($name, "umuai-nvl.csl");
             $this->outputJSON($pubs);
         } catch (Exception $e) {
             $error = array(
@@ -542,11 +317,10 @@ class APIController extends Controller
         }
     }
 
-	/**
-	 * 
-	 * @return multitype:multitype:string
-	 */
-	private function getUnAPIFormats()
+    /**
+     * @return array    The list of all available UNAPI formats
+     */
+    private function getUnAPIFormats()
 	{
 		
 		
@@ -611,8 +385,7 @@ class APIController extends Controller
 		// set the cache for the request
 		$this->app->etag('UnAPI/'.$id.$format);
 		$this->app->expires('+1 week');
-		
-		
+
 		$xml = null;
 	
 		if (!isset($id) && !isset($format))
@@ -624,27 +397,26 @@ class APIController extends Controller
 		try {
 
 			if (!isset($id))
-				throw new Exception('Bad Request - id is needed', 400);
+				throw new Exception('Argument \'id\' is required', 400);
 				
 			$ftdata = null;
 			if (isset($format))
 			{
 				$ftdata = $this->searchItem($this->getUnAPIFormats(),"name",$format);
 				if ( !$ftdata )
-					throw new Exception('Format Not Acceptable', 406);
+					throw new Exception('Argument \'format\': format not acceptable', 405);
 			}
 				
 			if (isset($id) && isset($format))
 			{
 				$item = $this->readZoteroCache($id);
-				
-				
+
 				if (!isset($item))
 					throw new Exception('Id Not Found', 404);
 				$content = $item[$format];
 				
 				if (!isset($content))
-					throw new Exception('Format Not Acceptable', 406);
+					throw new Exception('Content Not Found', 404);
 				
 				//var_dump($item);die();
 				$response = $this->app->response();
@@ -661,8 +433,11 @@ class APIController extends Controller
 		
 		} 
 		catch (\Exception  $e) {
-			$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><error/>');
-			$this->outputXML($xml,$e->getCode());
+			$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><nvlAppError/>');
+            $xml->addChild("method",$req->getPathInfo());
+            $xml->addChild("errorString",$e->getMessage());
+            $xml->addChild("errorCode",$e->getCode());
+            $this->outputXML($xml,$e->getCode());
 		}
 	}
 }
