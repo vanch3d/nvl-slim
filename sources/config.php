185d087187c331b8c8e99092d4e24f94cc8aed5b
vanch3d <nicolas.github@calques3d.org>
2018-01-10 20:37:59 +0000

    restructured config to make container setup easier

--- sources/config.php
+++ sources/config.php
@@ -7,28 +7,33 @@
  */
 
 return [
-    // SLIM settings
     'settings' => [
+
+        // SLIM settings
         'httpVersion' => '1.1',
         'responseChunkSize' => 4096,
         'outputBuffering' => 'append',
         'determineRouteBeforeAppMiddleware' => true,
         'displayErrorDetails' => getenv('APP_DEBUG') === 'true',
-    ],
 
-    // LOGGER settings
-    'logger' => [
-        'directory' => '/../.logs',
-        'filename' => 'my-app.log',
-        'timezone' => 'Europe/London',
-        'level' => 'debug',
-        'handlers' => []
-    ],
+        // LOGGER settings
+        'logger' => [
+            'directory' => __DIR__ . '/../.logs',
+            'filename' => 'my-app.log',
+            'timezone' => 'Europe/London',
+            'level' => Monolog,
+            'handlers' => []
+        ],
 
-    // TWIG settings
-    'twig' => [
-        'debug' => getenv('APP_DEBUG') === 'true',
-        'cache' => __DIR__ . '/../.cache/',
-    ]
+        // TWIG settings
+        'view' => [
+            'template_path' =>  __DIR__ . '/../resources/templates/',
+            'twig' => [
+                'debug' => getenv('APP_DEBUG') === 'true',
+                'cache' => __DIR__ . '/../.cache/',
+                'auto_reload' => getenv('APP_DEBUG') === 'true',
+            ],
+        ]
+    ],
 
 ];
