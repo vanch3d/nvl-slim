ffb0a86d7f26be21b99ce7e7137157ab54cd9fc0
vanch3d <nicolas.github@calques3d.org>
2018-01-11 11:41:56 +0000

    fixed dependencies improved gulp support (images, debug) a bit of cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,81 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var nano = require('gulp-cssnano');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var autoprefixer = require('gulp-autoprefixer');
-var apidoc = require('gulp-apidoc');
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+const sass = require('gulp-sass');
+const nano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const apidoc = require('gulp-apidoc');
+const image = require('gulp-image');
+//const autoprefixer = require('gulp-autoprefixer');
 
 var config = {
     sassPath: './resources/sass',
-    bowerDir: './bower_components'
+    bowerDir: './bower_components',
+    resDir:   './resources/assets',
+    destDir:  './public'
 }
 
-var scripts = [
+var jsList = [
     config.bowerDir + '/jquery/dist/jquery.js',
-	config.bowerDir + "/bootstrap/dist/js/bootstrap.js",
-    config.bowerDir + "/multi-pushmenu/js/classie.js",
-    config.bowerDir + "/multi-pushmenu/js/modernizr.custom.js",
-    config.bowerDir + "/multi-pushmenu/js/mlpushmenu.js",
-	"resources/assets/scripts/app.js"
+	config.bowerDir + '/bootstrap/dist/js/bootstrap.js',
+    config.bowerDir + '/multi-pushmenu/js/classie.js',
+    config.bowerDir + '/multi-pushmenu/js/modernizr.custom.js',
+    config.bowerDir + '/multi-pushmenu/js/mlpushmenu.js',
+    config.resDir   + '/scripts/app.js'
+];
+
+var cssList = [
+    config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
+    config.bowerDir + '/font-awesome/css/font-awesome.css',
+    config.bowerDir + '/academicons/css/academicons.css',
+    config.bowerDir + '/multi-pushmenu/css/component.css',
+    config.bowerDir + '/multi-pushmenu/css/icons.css',
+    config.resDir   + '/styles/app.scss'
+];
+
+var iconList = [
+    config.bowerDir + '/font-awesome/fonts/**.*',
+    config.bowerDir + '/academicons/fonts/**.*',
+    config.bowerDir + '/multi-pushmenu/fonts/**/**/*.*'
 ];
 
 gulp.task('scripts', function () {
-	return gulp.src(scripts)
+	return gulp.src(jsList)
+        .pipe(debug({title: 'script:'}))
 		.pipe(concat('app.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./public/js'))
+        .pipe(gulp.dest('./public/js'))
 });
 
 gulp.task('styles', function () {
-	return gulp.src([
-    	'bower_components/bootstrap/dist/css/bootstrap.css',
-        'bower_components/font-awesome/css/font-awesome.css',
-        'bower_components/academicons/css/academicons.css',
-        config.bowerDir + "/multi-pushmenu/css/component.css",
-        config.bowerDir + "/multi-pushmenu/css/icons.css",
-		'resources/assets/styles/app.scss'
-	])
-	.pipe(sass())
-	.pipe(nano())
-	.pipe(concat('app.css'))
-	.pipe(gulp.dest('./public/css'))
+	return gulp.src(cssList)
+        .pipe(debug({title: 'stylesheet:'}))
+        .pipe(sass())
+        .pipe(nano())
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('./public/css'))
 });
 
 gulp.task('icons', function() {
-    return gulp.src([
-        config.bowerDir + '/font-awesome/fonts/**.*',
-        config.bowerDir + '/academicons/fonts/**.*',
-        config.bowerDir + '/multi-pushmenu/fonts/**/**/*.*'
-    ])
+    return gulp.src(iconList)
+        .pipe(debug({title: 'icons:'}))
         .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('images', function () {
+    gulp.src(config.resDir + '/images/**/**/*.*')
+        .pipe(image({
+            concurrent: 10
+        }))
+        .pipe(gulp.dest('./public/images'));
 });
 
 gulp.task('apidoc', function(done){
     apidoc({
-        src: "./",
-        dest: "apidoc/",
+        src: './',
+        dest: 'apidoc/',
         // debug: true,
-        includeFilters: [ ".*\\.php$" ]
+        includeFilters: [ '.*\\.php$' ]
     }, done);
 });
 
