8ea6838d28ae89decd938d2bd05f1350a7fb3852
vanch3d <nicolas.github@calques3d.org>
2018-01-13 19:56:25 +0000

    added APIs for project data made "not implemented" notice more suitable for API consumption

--- sources/NVL/Controllers/APIController.php
+++ sources/NVL/Controllers/APIController.php
@@ -19,57 +19,83 @@ class APIController extends Controller
 
     public function apiHome(Request $request, Response $response, array $args)
     {
-        // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
-
+        return $response->withJson([ 'message' => 'Nothing to see here'], 200);
     }
 
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param array    $args
+     * @return Response
+     */
     public function unAPI(Request $request, Response $response, array $args)
     {
         // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        return $response->withJson([ 'error' => 'not yet implemented'], 403);
 
     }
 
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param array    $args
+     * @return Response
+     */
     public function getAllProjects(Request $request, Response $response, array $args)
     {
-        // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        $projects = [];
+        try {
+            $projects = $this->getProjectManager()->getData(false);
+        } catch (\Exception $e) {
+            $response = $response->withStatus(202,"Could not access list of projects");
+        }
+        return $response->withJson(['projects' => $projects]);
 
     }
 
     public function getAllPublications(Request $request, Response $response, array $args)
     {
         // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        return $response->withJson([ 'error' => 'not yet implemented'], 403);
 
     }
 
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param array    $args
+     * @return Response
+     */
     public function getProject(Request $request, Response $response, array $args)
     {
-        // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        $project = $this->getProjectManager()->isDefined($args["name"]);
+        if ($project)
+            return $response->withJson([ 'project' => $project], 200);
+        else
+            return $response->withJson(
+                ['error' => 'The project "' . $args["name"] . '" does not exist'],
+                404);
 
     }
 
     public function getPublications(Request $request, Response $response, array $args)
     {
         // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        return $response->withJson([ 'error' => 'not yet implemented'], 403);
 
     }
 
     public function getSlides(Request $request, Response $response, array $args)
     {
         // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        return $response->withJson([ 'error' => 'not yet implemented'], 403);
 
     }
 
     public function getImages(Request $request, Response $response, array $args)
     {
         // @todo[vanch3d] Build the proper response
-        return $response->withJson([ 'foo' => 'bar'], 200);
+        return $response->withJson([ 'error' => 'not yet implemented'], 403);
 
     }
 
