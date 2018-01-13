b8030334008de72b19bd9ca15a45b6faa79af204
vanch3d <nicolas.github@calques3d.org>
2018-01-13 19:54:07 +0000

    added project data handlers

--- sources/NVL/Data/ProjectManager.php
+++ sources/NVL/Data/ProjectManager.php
@@ -8,16 +8,24 @@
 
 namespace NVL\Data;
 
+use Couchbase\ViewQueryEncodable;
+use Exception;
 use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Slim\Container;
+use Slim\Exception\ContainerException;
+use Slim\Exception\ContainerValueNotFoundException;
 use Slim\Views\Twig;
 
 
 class ProjectManager
 {
-    protected $view = null;
+    protected $container = null;
 
     public function __construct(ContainerInterface $container)
     {
+        $this->container = $container;
     }
 
     /**
@@ -25,7 +33,7 @@ class ProjectManager
      * @param bool $short   false (default) to retrieve the whole description, true
      *                      to extract only the basic identifiers (id, name, url, start date)
      * @return array        An array containing the projects' description
-     * @throws \Exception
+     * @throws Exception
      * @todo                caching (see PHPSocialNetwork/phpFastCache)
      * @todo                project name needs to be redefined within each twig template
      */
@@ -33,9 +41,61 @@ class ProjectManager
     {
         $json = array();
 
-        throw new \Exception("TODO");
+        try {
+             /** @var Twig $view */
+             $view = $this->container->get("view");
+             $path = $view->getLoader()->getPaths()[0];
+
+            foreach(glob($path . '/projects/content/*.twig') as $file) {
+
+                $projid = basename($file,".twig");
+                $renderedTemplate = $view->fetch("/projects/content/".basename($file), array(
+                    'tmpl_base' => 'template.json.twig',
+                    'project' => array(
+                        "id" => $projid,
+                        "name"=>$projid)
+                ));
+                $data = json_decode($renderedTemplate,true);
+                if ($short)
+            {
+                    $filter = array("id","name","url","start");
+                    $data = array_intersect_key($data, array_flip($filter));
+                }
+                $json[$projid] = $data;
+            }
+            // sort projects by reverse chronological order (based on start date)
+            usort($json,function($a,$b){
+                return strcmp($b["start"], $a["start"]);
+            });
+
+        } catch (NotFoundExceptionInterface $e) {
+            // @todo[vanch3d] Better create our own exceptions
+            throw new Exception("");
+        } catch (ContainerExceptionInterface $e) {
+            throw new Exception("");
+        }
 
         return $json;
+    }
+
+    /**
+     *
+     * @param string $id  The id of the project to check
+     * @return mixed        False if the project does not exist, its descriptor if it does
+     */
+    public function isDefined($id)
+    {
+        if (empty($id)) return false;
+        try {
+            $projIdx = $this->getData();
+            $found = current(array_filter($projIdx, function($item) use($id) {
+                return isset($item['id']) && $id == $item['id'];
+            }));
+            return $found;
+
+        } catch (Exception $e) {
+            return false;
+        }
     }
 
 }
