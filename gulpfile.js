091806be823b9482790a542ac58a0f6c51115753
vanch3d <nicolas.github@calques3d.org>
2018-01-15 11:43:05 +0000

    added libraries to the JS/CSS bundles added support for non-bundled plugins added min suffix to minified bundles

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,93 @@
 const del = require('del');
 const gulp = require('gulp');
 const debug = require('gulp-debug');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const nano = require('gulp-cssnano');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const apidoc = require('gulp-apidoc');
 const image = require('gulp-image');
+
 //const autoprefixer = require('gulp-autoprefixer');
 
 var config = {
-    sassPath: './resources/sass',
     bowerDir: './bower_components',
     resDir:   './resources/assets',
     destDir:  './public'
-}
+};
 
+// list of core JS files to combine
 var jsList = [
     config.bowerDir + '/jquery/dist/jquery.js',
 	config.bowerDir + '/bootstrap/dist/js/bootstrap.js',
     config.bowerDir + '/multi-pushmenu/js/classie.js',
     config.bowerDir + '/multi-pushmenu/js/modernizr.custom.js',
     config.bowerDir + '/multi-pushmenu/js/mlpushmenu.js',
+    config.bowerDir + '/i18next/i18next.js',
+    config.bowerDir + '/fitvids/jquery.fitvids.js',
+    config.bowerDir + '/jquery-oembed-all/jquery.oembed.js',
+    config.bowerDir + '/jquery.easing/js/jquery.easing.js',
+    config.bowerDir + '/Snap.svg/dist/snap.svg.js',
+    config.bowerDir + '/holderjs/holder.js',
+    config.bowerDir + '/isotope-layout/dist/isotope.pkgd.js',
     config.resDir   + '/scripts/app.js'
 ];
 
+// list of JS plugins to uglify but keep separate
+var jsPluginList = [
+    config.resDir + '/_plugins/nvl-galleria/nvl-galleria.js'
+];
+
+// list of CSS files to combine
 var cssList = [
     config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
     config.bowerDir + '/font-awesome/css/font-awesome.css',
     config.bowerDir + '/academicons/css/academicons.css',
     config.bowerDir + '/multi-pushmenu/css/component.css',
     config.bowerDir + '/multi-pushmenu/css/icons.css',
-    config.resDir   + '/styles/app.scss'
+    config.resDir   + '/styles/app.scss',
+    config.resDir   + '/_plugins/nvl-galleria/css/nvl-galleria.css',
+
 ];
 
+// list of font & font-icons to copy
 var iconList = [
     config.bowerDir + '/font-awesome/fonts/**.*',
     config.bowerDir + '/academicons/fonts/**.*',
     config.bowerDir + '/multi-pushmenu/fonts/**/**/*.*'
 ];
 
+// list of images to process and copy
+var imgList = [
+    config.resDir + '/images/**/**/*.*',
+    config.resDir + '/_plugins/nvl-galleria/images/**/**/*.*',
+];
+
 gulp.task('scripts', function () {
+    gulp.src(jsPluginList)
+        .pipe(debug({title: 'plugin:'}))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./public/js'));
+
 	return gulp.src(jsList)
         .pipe(debug({title: 'script:'}))
 		.pipe(concat('app.js'))
+        .pipe(gulp.dest('./public/js'))
 		.pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./public/js'))
 });
 
 gulp.task('styles', function () {
-	return gulp.src(cssList)
+    return gulp.src(cssList)
         .pipe(debug({title: 'stylesheet:'}))
         .pipe(sass())
         .pipe(nano())
         .pipe(concat('app.css'))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./public/css'))
 });
 
@@ -64,7 +98,7 @@ gulp.task('icons', function() {
 });
 
 gulp.task('images', function () {
-    gulp.src(config.resDir + '/images/**/**/*.*')
+    return gulp.src(imgList)
         .pipe(image({
             concurrent: 10
         }))
