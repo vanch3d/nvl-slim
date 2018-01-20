3f71e1106dbf42ccca59f331230a60409df52887
vanch3d <nicolas.github@calques3d.org>
2018-01-20 13:57:40 +0000

    added pubReader's cite & note as Twig custom functions

--- sources/NVL/Support/Twig/WidgetExtension.php
+++ sources/NVL/Support/Twig/WidgetExtension.php
@@ -26,7 +26,15 @@ class WidgetExtension extends \Twig_Extension
             'get_doi' =>
                 new \Twig_SimpleFunction('doi', function (string $url) {
                     return "<a href='//dx.doi.org/$url' title=\"Go to article through CrossRef's DOI\">DOI</a>";
-                })
+                }),
+            'pubreader_cite' =>
+                new \Twig_SimpleFunction('cite', function (string $ref,string $id) {
+                    return "<a class='bibr tag_bib' rid='$id' href='#$id' co-class='co-refbox' co-rid='$id'>$ref</a>";
+                },array('is_safe' => array('html'))),
+            'pubreader_note' =>
+                new \Twig_SimpleFunction('note', function (string $ref,string $id) {
+                    return "<a class='bibr tag_note' rid='$id' href='#$id' co-class='co-refbox' co-rid='$id'>$ref</a>";
+                },array('is_safe' => array('html')))
         ];
     }
 
